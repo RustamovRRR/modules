06d04ad59f911e98a872252d012fc0aa417d4d60
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-08 20:12:52 +0400

    adding export default getElement module

--- setup/app.js
+++ setup/app.js
@@ -1,9 +1,10 @@
 import { random, people } from "../modules/data.js";
 import showPeople from "../modules/showPeople.js";
+import getElement from "../modules/getElement.js";
 
 console.log(random);
-const container = document.querySelector(".container");
-const btn = document.querySelector(".btn");
+const container = getElement(".container");
+const btn = getElement(".btn");
 
 btn.addEventListener("click", () => {
   container.innerHTML = showPeople(people);
