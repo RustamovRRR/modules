44a737954e11f5a14ddc3e58a1bfe3b64b2db65a
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-08 19:40:36 +0400

    export default showPeople functions

--- setup/app.js
+++ setup/app.js
@@ -1,19 +1,10 @@
 import { random, people } from "../modules/data.js";
+import showPeople from "../modules/showPeople.js";
 
 console.log(random);
 const container = document.querySelector(".container");
 const btn = document.querySelector(".btn");
 
-const showPeople = () => {
-  const newPeople = people
-    .map((person) => {
-      const { name, job } = person;
-      return `<p>${name} <strong>${job}</strong></p>`;
-    })
-    .join("");
-  container.innerHTML = newPeople;
-};
-
 btn.addEventListener("click", () => {
-  showPeople();
+  container.innerHTML = showPeople(people);
 });
